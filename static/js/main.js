94ac2978e7b0ebec9890bf883ef38da45220c1c5
Jeremy Vuillermet <jvu@zenexity.com>
2013-02-25 22:56:07 +0100

    change layout + ajax call

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('.text').on("click", function(e) {
+    var lamp = $(e.currentTarget)
+    toggle(lamp.data("light"), lamp)
+  });
+});
+
+function tog(st) { return st=="on" ? "off" : "on"}
+function toggle(state, lamp) {
+  var next = tog(state)
+  console.log(next)
+  lamp.removeClass(state).addClass(next)
+  lamp.data("light", next)
+  lamp.find("img").removeClass(state).addClass(next)
+  $.get('/'+lamp.attr('id')+'/'+next)
+}
