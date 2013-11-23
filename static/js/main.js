0e0f9a981874ed959f25516fafa0cd587d686cab
Jeremy <jeremy.vuillermet@gmail.com>
2013-11-24 00:15:43 +0100

    speech

--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,26 @@ $(function() {
       window.location = "/"
     }
   });
+
+  var recording = false
+  var recognition = new webkitSpeechRecognition();
+  recognition.lang = "fr-FR";
+  //recognition.continuous = true;
+  //recognition.interimResults = true;
+  recognition.onresult = function(e) {
+    console.log("toto", e.results[0][0].transcript)
+    $('#speech').append('<p>'+e.results[0][0].transcript+'</p>')
+  }
+  $('#voice').on("click", function(e) {
+    if(recording) {
+      recognition.stop();
+      recording = false;
+    } else {
+      console.log("starting")
+      recognition.start();
+      recording = true;
+    }
+  })
 });
 
 function tog(st) { return st=="on" ? "off" : "on"}
