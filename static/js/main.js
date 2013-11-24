77b7c458d9440ec16463fcc2243e639d37bb0adf
Jeremy <jeremy.vuillermet@gmail.com>
2013-11-24 16:59:57 +0100

    split name

--- static/js/main.js
+++ static/js/main.js
@@ -14,16 +14,27 @@ $(function() {
     }
   });
 
-  var keywords = [{word: "ALLUMER", action: "on"}, {word: "ETEINDRE", action: "off"}]
+  var keywords = [
+    {word: "ALLUMER", action: "on"}, 
+    {word: "ETEINDRE", action: "off"}, 
+    {word: "MONTER", action: "on"},
+    {word: "DESCENDRE", action: "off"},
+    {word: "OUVRIR", action: "on"},
+    {word: "FERMER", action: "off"}
+    ]
   var recording = false
   var recognition = new webkitSpeechRecognition();
   recognition.lang = "fr-FR";
   //recognition.continuous = true;
   //recognition.interimResults = true;
-
+console.log(_.map(pins, function(pi) {
+      return {id: pi.id, names: pi.name.split(" ")}
+    }))
 
   recognition.onresult = function(e) {
     var res = e.results[0][0].transcript.replace(/é/g, "e").toUpperCase();
+    
+    
     //$('#debug').append("<p>"+res+"</p>");
     var mbKey = _.find(keywords, function(k) {
       return _.contains(res, k.word)
@@ -31,12 +42,16 @@ $(function() {
 
     if(mbKey) {
       var pin = _.find(pins, function(p) {
-        return _.contains(res, p.name.toUpperCase())
+        return _.every(p.names, function(ap) {
+          return _.contains(res, ap);
+        })
       });
       //$('#debug').append("<p>"+'/'+pin.id+"/"+mbKey.action+"</p>")
-      $.get('/'+pin.id+"/"+mbKey.action)
+      if(pin) $.get('/'+pin.id+"/"+mbKey.action)
+      else $('#voice').text(res)
+    } else {
+      $('#voice').text(res)
     }
-
   }
 
   $('#voice').on("click", function(e) {
@@ -55,9 +70,12 @@ $(function() {
 function tog(st) { return st=="on" ? "off" : "on"}
 function toggle(state, lamp) {
   var next = tog(state)
-  console.log(next)
   lamp.removeClass(state).addClass(next)
   lamp.data("light", next)
   lamp.find("img").removeClass(state).addClass(next)
   $.get('/'+lamp.attr('id')+'/'+next)
+}
+
+function turnOff(lamp) {
+  console.log("toto")
 }
