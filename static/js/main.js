8247a2878b8c6923ffa4413557f096953f1a7950
Jeremy <jeremy.vuillermet@gmail.com>
2013-11-24 16:18:35 +0100

    speech

--- static/js/main.js
+++ static/js/main.js
@@ -14,15 +14,29 @@ $(function() {
     }
   });
 
+  var keywords = [{word: "ALLUMER", action: "on"}, {word: "ETEINDRE", action: "off"}]
   var recording = false
   var recognition = new webkitSpeechRecognition();
   recognition.lang = "fr-FR";
   //recognition.continuous = true;
   //recognition.interimResults = true;
+
   recognition.onresult = function(e) {
-    console.log("toto", e.results[0][0].transcript)
-    $('#speech').append('<p>'+e.results[0][0].transcript+'</p>')
+    var res = e.results[0][0].transcript.replace(/é/g, "e");
+
+    var mbKey = _.find(keywords, function(k) {
+      return _.contains(res, k.word)
+    })
+
+    if(mbKey) {
+      var pin = _.find(pins, function(p) {
+        return _.contains(res.toUpperCase(), p.name.toUpperCase())
+      });
+      $.get('/'+pin.id+"/"+mbKey.action)
+    }
+
   }
+  
   $('#voice').on("click", function(e) {
     if(recording) {
       recognition.stop();
